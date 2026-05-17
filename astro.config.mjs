2a9784b18b751a98fed21f8a4af3a8fd029d7b01
Tom VanAntwerp <tom@tomvanantwerp.com>
2026-05-17 08:53:23 -0400

    Update for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tvanantwerp.github.io',
-  base: '/militant-bicyclists',
+	site: 'https://militantbicyclists.com'
 });
